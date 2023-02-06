e0e75d435c33cbf7fa58c3f734d0e0c2c57ac4f2
BastienVitour <bvitour2004@gmail.com>
2023-02-06 15:00:56 +0100

    les ennemis sont bourrés mais bougent

--- app.js
+++ app.js
@@ -18,70 +18,112 @@ for (let i=1; i<241; i++) {
     grille.append(cases)
 }
 
+function goLeft() {
+    for (let i = 0; i < 239; i ++) {
+
+        let cases = document.querySelectorAll('.case')[i]
+
+        if (cases.previousSibling) {
+            if (cases.classList.contains('alien') && !cases.previousElementSibling.classList.contains('alien')) {
+                cases.classList.remove('alien')
+                cases.previousElementSibling.classList.add('alien')
+            }
+        }
+        
+    }
+}
+
+function goRight() {
+    for (let i = 239; i >= 0; i --) {
+
+        let cases = document.querySelectorAll('.case')[i]
+
+        if (cases.nextSibling) {
+            if (cases.classList.contains('alien') && !cases.nextElementSibling.classList.contains('alien')) {
+                cases.classList.remove('alien')
+                cases.nextElementSibling.classList.add('alien')
+            }
+        }
+        
+    }
+}
+
 let direction= 'right';
 
 function move() {
 
+    //console.log("playing function")
+
     let num = document.getElementsByClassName('alien').length
     let aliens = document.getElementsByClassName('alien')
 
+    let cases = document.querySelectorAll('.case')
+
     console.log(num)
+
+    if (cases[239].classList.contains('alien')) {
+        return 0;
+    }
 
     for (let k = num-1; k >= 0; k--) {
 
+
         if (aliens[k].classList.contains('right-stop')) {
+            
+            for(let j = 0; j < 20; j++) {
+                for (let i = 239; i >= 0; i --) {
 
+                    let cases = document.querySelectorAll('.case')[i]
+            
+                    if (cases.nextSibling) {
+                        if (cases.classList.contains('alien') && !cases.nextElementSibling.classList.contains('alien')) {
+                            cases.classList.remove('alien')
+                            cases.nextElementSibling.classList.add('alien')
+                        }
+                    }
+                    
+                }
+            }            
             direction = 'left';
-
+            break;  
+            
         }
 
-        else if (aliens[k].classList.contains('left-stop')) {
+        else if (aliens[k].classList.contains('left-stop') && !cases[0].classList.contains('alien')) {
+
+            for(let j = 0; j < 20; j++) {
+                for (let i = 239; i >= 0; i --) {
+
+                    let cases = document.querySelectorAll('.case')[i]
+            
+                    if (cases.nextSibling) {
+                        if (cases.classList.contains('alien') && !cases.nextElementSibling.classList.contains('alien')) {
+                            cases.classList.remove('alien')
+                            cases.nextElementSibling.classList.add('alien')
+                        }
+                    }
+                    
+                }
+            }
 
             direction = 'right';
+            break;
 
         }
-
     }
 
     if (direction == 'right') {
 
-        for (let i = 239; i >= 0; i --) {
-
-            let cases = document.querySelectorAll('.case')[i]
-    
-            if (cases.nextSibling) {
-                if (cases.classList.contains('alien') && !cases.nextElementSibling.classList.contains('alien')) {
-                    cases.classList.remove('alien')
-                    cases.nextElementSibling.classList.add('alien')
-                }
-            }
-            
-        }
+        goRight();
 
     }
 
     else if (direction == 'left') {
 
-        for (let i = 0; i < 239; i ++) {
-
-            let cases = document.querySelectorAll('.case')[i]
-    
-            if (cases.previousSibling) {
-                if (cases.classList.contains('alien') && !cases.previousElementSibling.classList.contains('alien')) {
-                    cases.classList.remove('alien')
-                    cases.previousElementSibling.classList.add('alien')
-                }
-            }
-            
-        }
+        goLeft();
 
     }
-
     
 }
 
-setInterval(move, 1000)
-
-//let cases = document.querySelectorAll('.alien')
-
-//move(cases)
+setInterval(move, 500)
