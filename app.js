8cf2f7218eff8cd901e6946c4daf8b690d098ff2
BastienVitour <bvitour2004@gmail.com>
2023-02-06 11:49:44 +0100

    ils vont a droite

--- app.js
+++ app.js
@@ -10,6 +10,39 @@ for (let i=1; i<241; i++) {
         cases.setAttribute('class', 'left-stop')
         cases.setAttribute('style', 'background-color:green;')
     }
-    
+    if (i < 61 && i%20 < 13 && i%20 != 0) {
+        cases.classList.add('alien')
+    }
+    cases.classList.add('case')
+    cases.setAttribute('id', i)
     grille.append(cases)
 }
+
+function move() {
+
+    let num = document.getElementsByClassName('case').length
+
+    console.log(num)
+
+    //for (let k = 1; k < 241; k++) {
+
+        for (let i = num-1; i >= 0; i --) {
+
+            let cases = document.querySelectorAll('.case')[i]
+            //console.log(cases.id)
+
+            if (cases.classList.contains('alien') && !cases.nextElementSibling.classList.contains('alien')) {
+                cases.classList.remove('alien')
+                cases.nextElementSibling.classList.add('alien')
+            }
+            //cases.setAttribute('style', 'background-color:yellow;')
+        }
+    //}
+}
+
+setInterval(move, 1000)
+
+
+//let cases = document.querySelectorAll('.alien')
+
+//move(cases)
