c2af50efbae8e38c035fdc9d6d4dc8e64cf9feea
BastienVitour <bvitour2004@gmail.com>
2023-02-07 10:46:03 +0100

    prettier

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ for (let i=1; i<241; i++) {
     let cases = document.createElement('div');
     if (i%20 == 0) {
         cases.setAttribute('class', 'right-stop')
-        cases.setAttribute('style', 'background-color:red;')
+        //cases.setAttribute('style', 'background-color:red;')
     }
     else if (i%20 == 1){
         cases.setAttribute('class', 'left-stop')
-        cases.setAttribute('style', 'background-color:green;')
+        //cases.setAttribute('style', 'background-color:green;')
     }
     if (i < 61 && i%20 < 13 && i%20 != 0) {
         cases.classList.add('alien')
