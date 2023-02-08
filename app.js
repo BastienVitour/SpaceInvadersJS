6c0ab5799a75868f2b6615eeec23c231b24b3cf6
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-02-08 15:45:27 +0100

    part of the secret

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var url = window.location.href;
+var isSecret = url.includes('secret');
+
 let grille = document.querySelector('.grille');
 
 for (let i=1; i<241; i++) {
@@ -10,17 +13,23 @@ for (let i=1; i<241; i++) {
     }
     if (i < 61 && i%20 < 13 && i%20 != 0) {
         cases.classList.add('alien')
+        if(isSecret) {
+            cases.classList.add('secret_alien')
+        }
     }
     if (i == 230) {
-        cases.classList.add('tireur')
+        if(isSecret) {
+            cases.classList.add('secret_tireur')
+        }
+        else {
+            cases.classList.add('tireur')
+        }
+       
     }
     cases.classList.add('case')
     cases.setAttribute('id', i)
     grille.append(cases)
 
-if (i == 230) {
-    cases.classList.add('tireur')
-}
 }
 
 function goLeft() {
@@ -29,6 +38,12 @@ function goLeft() {
         let cases = document.querySelectorAll('.case')[i]
 
         if (cases.previousSibling) {
+            if(isSecret) {
+                if (cases.classList.contains('secret_alien') && !cases.previousElementSibling.classList.contains('secret_alien')) {
+                    cases.classList.remove('secret_alien')
+                    cases.previousElementSibling.classList.add('secret_alien')
+                }
+            }
             if (cases.classList.contains('alien') && !cases.previousElementSibling.classList.contains('alien')) {
                 cases.classList.remove('alien')
                 cases.previousElementSibling.classList.add('alien')
@@ -44,6 +59,12 @@ function goRight() {
         let cases = document.querySelectorAll('.case')[i]
 
         if (cases.nextSibling) {
+            if(isSecret) {
+                if (cases.classList.contains('secret_alien') && !cases.nextElementSibling.classList.contains('secret_alien')) {
+                    cases.classList.remove('secret_alien')
+                    cases.nextElementSibling.classList.add('secret_alien')
+                }
+            }
             if (cases.classList.contains('alien') && !cases.nextElementSibling.classList.contains('alien')) {
                 cases.classList.remove('alien')
                 cases.nextElementSibling.classList.add('alien')
@@ -59,6 +80,10 @@ var lost = false;
 function move() {
     console.log(speed)
 
+    if(isSecret) {
+        let num = document.getElementsByClassName('secret_alien').length
+        let aliens = document.getElementsByClassName('secret_alien')
+    }
     let num = document.getElementsByClassName('alien').length
     let aliens = document.getElementsByClassName('alien')
 
@@ -84,7 +109,18 @@ function move() {
             break;  
             
         }
+        if(isSecret) {
+            if (aliens[k].classList.contains('left-stop') && !cases[0].classList.contains('secret_alien')) {
 
+                for(let j = 0; j < 20; j++) {
+                    goRight()
+                }
+    
+            direction = 'right';
+            break;
+    
+            }
+        }
         else if (aliens[k].classList.contains('left-stop') && !cases[0].classList.contains('alien')) {
 
             for(let j = 0; j < 20; j++) {
@@ -110,6 +146,28 @@ function move() {
     }
 
     for (let i = 0; i < casesList.length; i++) {
+        if(isSecret) {
+            if (casesList[i].classList.contains('secret_tireur')) {
+                console.log(casesList[i].classList)
+            }
+            if ((casesList[i].classList.contains('secret_tireur') && casesList[i].classList.contains('secret_alien')) || (casesList[i].classList.contains('secret_alien') && i > 220)) {
+                console.log('Vous avez perdu')
+                clearInterval(game)
+                lost = true;
+                alert('vous avez perdu')
+                break;
+            }
+            if (casesList[i].classList.contains('secret_tireur')) {
+                console.log(casesList[i].classList)
+            }
+            if ((casesList[i].classList.contains('secret_tireur') && casesList[i].classList.contains('alien')) || (casesList[i].classList.contains('alien') && i > 220)) {
+                console.log('Vous avez perdu')
+                clearInterval(game)
+                lost = true;
+                alert('vous avez perdu')
+                break;
+            }
+        }
         if (casesList[i].classList.contains('tireur')) {
             console.log(casesList[i].classList)
         }
@@ -136,6 +194,15 @@ function goUp() {
                 }
                 
             }
+            if(isSecret) { 
+                if (cases[k].classList.contains('secret_laser')) {
+                    cases[k].classList.remove('secret_laser')
+                    if (cases[k].previousElementSibling) {
+                        cases[k].previousElementSibling.classList.add('secret_laser')
+                    }
+                    
+                }
+            }
         }
     }
     
@@ -146,26 +213,47 @@ function goUp() {
             setTimeout(function(){
                 cases[k].classList.remove('alien')
                 cases[k].classList.remove('boom')
-           }, 200);
+           }, 100);
 
+        }
+        if(isSecret) {
+            if (cases[k].classList.contains('secret_alien') && cases[k].classList.contains('secret_laser')) {
+                cases[k].classList.add('boom')
+                cases[k].classList.remove('secret_laser')
+                setTimeout(function(){
+                    cases[k].classList.remove('secret_alien')
+                    cases[k].classList.remove('boom')
+               }, 100);
+    
+            }
+            if (cases[k].classList.contains('alien') && cases[k].classList.contains('secret_laser')) {
+                cases[k].classList.add('boom')
+                cases[k].classList.remove('secret_laser')
+                setTimeout(function(){
+                    cases[k].classList.remove('alien')
+                    cases[k].classList.remove('boom')
+               }, 100);
+    
+            }
         }
     }
 }
 
 console.log(window.location.href)
 
-url = window.location.href;
-
 var speed;
 
 if (url.includes('easy')) {
-    speed = 1000;
-}
-else if (url.includes('mid')) {
     speed = 750;
 }
-else if (url.includes('hard')) {
+else if (url.includes('mid')) {
     speed = 500;
+}
+else if (url.includes('hard')) {
+    speed = 250;
+}
+else if (isSecret) {
+    speed = 250;
 }
 
 var game;
@@ -175,7 +263,12 @@ let launcher = document.getElementById('button');
 launcher.addEventListener("click", () => {
     launcher.style.display = 'none'
     game = setInterval(move, speed)
-    movement()
+    if (isSecret){
+        secretMovement()
+    }
+    else {
+        movement()
+    }
 });
 
 let stopper = document.getElementById('stop');
@@ -193,6 +286,68 @@ replay.addEventListener("click", () => {
 let casesList = document.querySelectorAll('.case')
 
 // PLAYER MOVEMENT
+
+function secretMovement() {
+    let tireur = document.querySelector('.secret_tireur')
+
+        document.onkeydown = function (e) {
+        let cases = document.querySelector('.secret_tireur')
+
+        if (e.key == 'ArrowUp') {
+                for(let j = 0; j < 20; j++) {
+
+                    let cases = document.querySelector('.secret_tireur')
+
+                    if (tireur.id >= 163){
+                
+                                cases.classList.remove('secret_tireur')
+                                cases.previousElementSibling.classList.add('secret_tireur')
+                                tireur.id = cases.id
+                    }        
+            }
+        }
+
+        if (e.key == 'ArrowDown') {
+            for(let j = 0; j < 20; j++) {
+
+                let cases = document.querySelector('.secret_tireur')
+
+                if (tireur.id <= 238){
+                
+                    cases.classList.remove('secret_tireur')
+                    cases.nextElementSibling.classList.add('secret_tireur')
+                    tireur.id = cases.id
+                }
+            }
+        }
+
+        if (e.key == 'ArrowLeft') {
+            if (!cases.classList.contains('left-stop')){
+                cases.classList.remove('secret_tireur')
+                cases.previousElementSibling.classList.add('secret_tireur')
+            }
+        }
+
+        if (e.key == 'ArrowRight'){
+            if (!cases.classList.contains('right-stop')){
+                cases.classList.remove('secret_tireur')
+                cases.nextElementSibling.classList.add('secret_tireur')
+            }
+        }
+
+        if (e.code == 'Space') {
+            console.log(e.code)
+            let cases = document.querySelectorAll('.case')
+
+            for (let i = 0; i < cases.length; i++) {
+                if (cases[i].classList.contains('secret_tireur')) {
+                    cases[i].classList.add('secret_laser')
+                }
+            }
+            setInterval(goUp, 600)
+        }
+    };
+}
 
 function movement() {
     let tireur = document.querySelector('.tireur')
@@ -273,23 +428,20 @@ function isEqual(tableau1, tableau2) {
 document.onkeydown = function (f) {
     if (f.key == 'ArrowUp') {
         code.push("UP")
-        console.log(code)
 
     }
     if (f.key == 'ArrowDown') {
         code.push("DOWN")
-        console.log(code)
     }
     if (f.key == 'ArrowRight') {
         code.push("RIGHT")
-        console.log(code)
     }
     if (f.key == 'ArrowLeft') {
         code.push("LEFT")
-        console.log(code)
     }
     if (code.length == rightCode.length) {
         if (isEqual(code, rightCode) == true){
+            window.location.href = 'game.html?diff=secret'
             alert("vous avez trouvé le niveau secret")
             for (let f = 0; f < rightCode.length; f++){
                 code.pop()
@@ -299,7 +451,6 @@ document.onkeydown = function (f) {
             for (let f = 0; f < rightCode.length; f++){
                 code.pop()
             }
-            console.log(code)
             alert("tu t'es trompé")
         }
     }
